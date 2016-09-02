d8fc650f54818b0c7adf66483b04bde65d4e9f9c
丁玉松 <dys90@qq.com>
2016-09-02 16:45:05 +0800

    init commit

---
+++ OC_JSInteractionDemo/JSCallOCViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSCallOCViewController.h
+//  OC_JSInteractionDemo
+//
+//  Created by 丁玉松 on 16/9/2.
+//  Copyright © 2016年 Beijing Yingyan Internet Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSCallOCViewController : UIViewController
+
+@end
---
+++ OC_JSInteractionDemo/OCCallJSViewController.m
@@ -0,0 +1,37 @@
+//
+//  OCCallJSViewController.m
+//  OC_JSInteractionDemo
+//
+//  Created by 丁玉松 on 16/9/2.
+//  Copyright © 2016年 Beijing Yingyan Internet Co., Ltd. All rights reserved.
+//
+
+#import "OCCallJSViewController.h"
+
+@interface OCCallJSViewController ()
+
+@end
+
+@implementation OCCallJSViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
